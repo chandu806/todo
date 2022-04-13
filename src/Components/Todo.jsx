44de9c2f4f36cdb8cc2957ae01751f66e307f785
chandu <daanechandu@gmail.com>
2022-04-13 11:47:39 +0530

    todo

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+
+export const Todo = () => {
+  const [formDetails, setFormDetails] = React.useState({
+    name : "",
+    email: "",
+    password: "",
+    username: "",
+    mobile: "",
+    description: ""
+  });
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    console.log(e)
+    fetch("https://masai-api-mocker.herokuapp.com/auth/register", {
+      method:"POST",
+      mode: "no-cors",
+      body:JSON.stringify(formDetails),
+      headers: {"Content-Type" : "application/json"}
+    })
+     .then((res) => (res.json()))
+     .then((res) => (setFormDetails(res)))
+
+     .catch((err) => (console.log(err)))
+  }
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+      <input placeholder="Names" type="text"></input>
+      <br />
+      <input placeholder="fruits" type="text"></input>
+      <br />
+      <input placeholder="Vegetables" type="text"></input>
+      <br />
+      <input type="submit" placeholder="Save"></input>
+
+      </form>
+    </div>
+  ) 
+}
